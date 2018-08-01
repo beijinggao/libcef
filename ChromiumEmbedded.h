0b67d04eb85c7bc33a6c5d7ddcae89a2a40a3060
gao8954 <895407214gao@sina.com>
2018-08-02 00:08:52 +0800

    libcef

---
+++ ChromiumEmbedded.h
@@ -0,0 +1,19 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#include "include/capi/cef_app_capi.h"
+#include "include/capi/cef_base_capi.h"
+#include "include/capi/cef_client_capi.h"
+#include "include/capi/cef_command_line_capi.h"
+#include "include/capi/cef_request_capi.h"
+#include "include/capi/cef_response_capi.h"
+#include "include/capi/cef_scheme_capi.h"
+#include "include/capi/cef_geolocation_capi.h"
+#include "include/capi/cef_origin_whitelist_capi.h"
+#include "include/capi/cef_parser_capi.h"
+#include "include/capi/cef_path_util_capi.h"
+#include "include/capi/cef_zip_reader_capi.h"
+#include "include/capi/cef_xml_reader_capi.h"
+#include "include/capi/cef_urlrequest_capi.h"
+#include "include/capi/cef_trace_capi.h"
---
+++ Package.swift
@@ -0,0 +1,7 @@
+import PackageDescription
+
+let package = Package(
+	name: "ChromiumEmbedded",
+	//pkgConfig: "libcurl",
+	//providers: [.Brew("curl"), .Apt("libcurl4-openssl-dev")]
+)
